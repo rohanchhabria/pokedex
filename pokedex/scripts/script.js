063fa19a710269ca668dba869e5e842cdef0d3b8
Rohan Chhabria <58826613+rohanchhabria@users.noreply.github.com>
2021-07-17 20:19:40 +0530

    Update script.js

--- pokedex/scripts/script.js
+++ pokedex/scripts/script.js
@@ -13,11 +13,11 @@
 </div> */
 
 const pokemoncontainer = document.querySelector('.pokemon-container');
-const pokemonavatar = document.querySelector('.pokemon-avatar');
-const pokemonnumber = document.querySelector('.pokemon-number');
-const pokemonname = document.querySelector('.pokemon-name');
-const pokemontype = document.querySelector('.pokemon-type');
-const pokemonability = document.querySelector('.pokemon-ability');
+const pokemonavatar = document.querySelectorAll('.pokemon-avatar');
+const pokemonnumber = document.querySelectorAll('.pokemon-number');
+const pokemonname = document.querySelectorAll('.pokemon-name');
+const pokemontype = document.querySelectorAll('.pokemon-type');
+const pokemonability = document.querySelectorAll('.pokemon-ability');
 
 const animateeffect = document.querySelectorAll('.animate-effect');
 const animatetext = document.querySelectorAll('.animate-text');
@@ -54,11 +54,17 @@ const getpokemon = async(id) => {
     pokemoncard(data);
 }
 
+let pokeid, pokenumber, pokename, allabilities, alltypes;
+
 const pokemoncard = (pokemondata) => {
     const pokemon = document.createElement('div');
     pokemon.classList.add('pokemon');
 
-    let allabilities = `Abilites for ${pokemondata.name} are `;
+    pokeid = pokemondata.id;
+    pokenumber = pokemondata.id.toString().padStart(3, '0');
+    pokename = pokemondata.name;
+
+    allabilities = `Abilites for ${pokemondata.name} are `;
     // console.log(pokemondata.abilities);
     for(ability in pokemondata.abilities) {
         // console.log(data.abilities[ability].ability.name);
@@ -68,53 +74,102 @@ const pokemoncard = (pokemondata) => {
     allabilities = allabilities.substring(0, allabilities.length - 2) + '.';
     // console.log(allabilities);
 
-    let alltypes = pokemondata.types.map(type => type.type.name)[0];
+    alltypes = pokemondata.types.map(type => type.type.name)[0];
     // console.log(alltypes);
     // console.log(colortypes);
 
     // pokemon.style.backgroundColor = cardcolors[alltypes];
 
+        // pokemon.innerHTML = `<div class="pokemon-avatar animate-effect">
+        //     <img src="https://pokeres.bastionbot.org/images/pokemon/${pokeid}.png" alt="${pokeid}" />
+        // </div>
+        // <div class="pokemon-info">
+        //     <span class="pokemon-number animate-effect animate-text">#${pokenumber}</span>
+        //     <h3 class="pokemon-name animate-effect animate-text">${pokename}</h3>
+        //     <small class="pokemon-type animate-effect animate-text">type: <span>${alltypes}</span></small>
+        // </div>
+        // <div class="pokemon-ability">${allabilities}</div>`;
+
     pokemon.innerHTML = `
-        <div class="pokemon-avatar">
-            <img src="https://pokeres.bastionbot.org/images/pokemon/${pokemondata.id}.png" alt="bulbasaur" />
+        <div class="pokemon-avatar animate-effect">
+            &nbsp;
         </div>
         <div class="pokemon-info">
-            <span class="pokemon-number">#${pokemondata.id.toString().padStart(3, '0')}</span>
-            <h3 class="pokemon-name">${pokemondata.name}</h3>
-            <small class="pokemon-type">type: <span>${alltypes}</span></small>
+            <span class="pokemon-number animate-effect animate-text">&nbsp;</span>
+            <h3 class="pokemon-name animate-effect animate-text">&nbsp;</h3>
+            <small class="pokemon-type animate-effect animate-text">&nbsp;</small>
         </div>
         <div class="pokemon-ability">${allabilities}</div>`;
 
     pokemoncontainer.appendChild(pokemon);
 }
 
-fetchpokemons();
+// fetchpokemons();
+
+// function renderdata() {
+//     fetchpokemons();
+
+//     const pokemonavatar = document.querySelector('.pokemon-avatar');
+//     const pokemonnumber = document.querySelector('.pokemon-number');
+//     const pokemonname = document.querySelector('.pokemon-name');
+//     const pokemontype = document.querySelector('.pokemon-type');
+//     const pokemonability = document.querySelector('.pokemon-ability');
+
+
+//     pokemonavatar.forEach((element) => {
+//         element.innerHTML = '<img src="https://pokeres.bastionbot.org/images/pokemon/1.png" alt="bulbasaur" />';
+//     });
+    
+//     pokemonnumber.forEach((element) => {
+//         element.innerHTML = '#001';
+//     });
+
+//     pokemonname.forEach((element) => {
+//         element.innerHTML = 'bulbasaur';    
+//     });
+    
+//     pokemontype.forEach((element) => {
+//         element.innerHTML = `type: <span>grass</span>`;
+//     });
+
+//     pokemonability.forEach((element) => {
+//         element.innerHTML = 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores, eius.';
+//         element.style.display = 'block';
+//     });
+
+//     animateeffect.forEach((element) => element.classList.remove('animate-effect'));
+//     animatetext.forEach((element) => element.classList.remove('animate-text'));
+// }
 
 function renderdata() {
-    // pokemonavatar.forEach((element) => {
-    //     element.innerHTML = '<img src="https://pokeres.bastionbot.org/images/pokemon/1.png" alt="bulbasaur" />';
-    // });
+    fetchpokemons();
+
+    pokemonavatar.forEach((element) => {
+        console.log(element.innerHTML);
+        element.innerHTML = `<img src="https://pokeres.bastionbot.org/images/pokemon/${pokeid}.png" alt="${pokeid}" />`;
+    });
     
-    // pokemonnumber.forEach((element) => {
-    //     element.innerHTML = '#001';
-    // });
+    pokemonnumber.forEach((element) => {
+        element.innerHTML = '#' + pokenumber;
+        console.log(pokenumber);
+    });
 
-    // pokemonname.forEach((element) => {
-    //     element.innerHTML = 'bulbasaur';    
-    // });
+    pokemonname.forEach((element) => {
+        element.innerHTML = '';    
+    });
     
-    // pokemontype.forEach((element) => {
-    //     element.innerHTML = `type: <span>grass</span>`;
-    // });
+    pokemontype.forEach((element) => {
+        element.innerHTML = '';
+    });
 
-    // pokemonability.forEach((element) => {
-    //     element.innerHTML = 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores, eius.';
-    //     element.style.display = 'block';
-    // });
+    pokemonability.forEach((element) => {
+        element.innerHTML = '';
+        element.style.display = 'block';
+    });
 
-    // animateeffect.forEach((element) => element.classList.remove('animate-effect'));
-    // animatetext.forEach((element) => element.classList.remove('animate-text'));
+    animateeffect.forEach((element) => element.classList.remove('animate-effect'));
+    animatetext.forEach((element) => element.classList.remove('animate-text'));
 }
 
 // this will render the card data after two seconds.
-// setTimeout(renderdata, 2000)
+setTimeout(renderdata, 2000)
